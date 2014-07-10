27f87ad1f01933b98c48a31c0e724e3e470d2c60
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-07-10 17:50:56 +0200

    adding more methods

--- mosquitto.php
+++ mosquitto.php
@@ -64,4 +64,150 @@ class Client
      *                           more information. If NULL, the default set will be used.
      */
     public function setTlsOptions($certReqs, $tlsVersion, $cipers) {}
+
+    /**
+     * Configure the client for pre-shared-key based TLS support. Must be called before connect(). Cannot be used in
+     * conjunction with setTlsCertificates.
+     *
+     * @param string    $psk        The pre-shared key in hex format with no leading "0x".
+     * @param string    $identity   The identity of this client. May be used as the username depending on server
+     *                              settings.
+     * @param string    $cipers     A string describing the ciphers available for use. See the openssl ciphers tool
+     *                              for more information. If NULL, the default set will be used.
+     */
+    public function setTlsPSK($psk, $identity, $cipers) {}
+
+    /**
+     * Set the client "last will and testament", which will be sent on an unclean disconnection from the broker.
+     * Must be called before connect().
+     *
+     * @param string    $topic      The topic on which to publish the will.
+     * @param string    $payload    The data to send.
+     * @param int       $qos        Optional. Default 0. Integer 0, 1, or 2 indicating the Quality of Service to be used.
+     * @param boolean   $retain     Optional. Default false. If true, the message will be retained.
+     */
+    public function setWill($topic, $payload, $qos = 0, $retain = false) {}
+
+    /**
+     * Remove a previously-set will. No parameters.
+     */
+    public function clearWill() {}
+
+    /**
+     * Control the behaviour of the client when it has unexpectedly disconnected in loopForever. The default
+     * behaviour if this method is not used is to repeatedly attempt to reconnect with a delay of 1 second until the
+     * connection succeeds.
+     *
+     * @param int       $reconnect_delay        Set delay between successive reconnection attempts.
+     * @param int       $reconnect_delay_exp    Set max delay between successive reconnection attempts when
+     *                                          exponential backoff is enabled
+     * @param boolean   $exponential_backoff    Enable exponential backoff
+     */
+    public function setReconnectDelay($reconnect_delay, $reconnect_delay_exp, $exponential_backoff) {}
+
+    /**
+     * Connect to an MQTT broker.
+     *
+     * @param string    $host       Hostname to connect to
+     * @param int       $port       Optional. Port number to connect to. Defaults to 1883.
+     * @param int       $keepalive  Optional. Number of sections after which the broker should PING the client if no
+     *                              messages have been received.
+     * @param string    $interface  Optional. The address or hostname of a local interface to bind to for this connection.
+     */
+    public function connect($host, $port = 1883, $keepalive = null, $interface = null) {}
+
+    /**
+     * Disconnect from the broker. No parameters.
+     */
+    public function disconnect() {}
+
+    /**
+     * Set the connect callback. This is called when the broker sends a CONNACK message in response to a connection.
+     *
+     * @param callback $callback    The callback
+     * @link https://github.com/mgdm/Mosquitto-PHP#onconnect
+     */
+    public function onConnect($callback) {}
+
+    /**
+     * Set the disconnect callback. This is called when the broker has received the DISCONNECT command and has
+     * disconnected the client.
+     *
+     * @param callback $callback The callback
+     * @link https://github.com/mgdm/Mosquitto-PHP#ondisconnect
+     */
+    public function onDisconnect($callback) {}
+
+    /**
+     * Set the logging callback.
+     *
+     * @param callback $callback The callback
+     * @link https://github.com/mgdm/Mosquitto-PHP#onlog
+     */
+    public function onLog($callback) {}
+
+    /**
+     * Set the subscribe callback. This is called when the broker responds to a subscription request.
+     *
+     * @param callback $callback The callback
+     * @link https://github.com/mgdm/Mosquitto-PHP#onsubscribe
+     */
+    public function onSubscribe($callback) {}
+
+    /**
+     * Set the unsubscribe callback. This is called when the broker responds to a unsubscribe request.
+     *
+     * @param callback $callback The callback
+     * @link https://github.com/mgdm/Mosquitto-PHP#onunsubscribe
+     */
+    public function onUnsubscribe($callback) {}
+
+    /**
+     * Set the message callback. This is called when a message is received from the broker.
+     *
+     * @param callback $callback The callback
+     * @link https://github.com/mgdm/Mosquitto-PHP#onmessage
+     */
+    public function onMessage($callback) {}
+
+    /**
+     * Set the number of QoS 1 and 2 messages that can be “in flight” at one time. An in flight message is part way
+     * through its delivery flow. Attempts to send further messages with publish() will result in the messages being
+     * queued until the number of in flight messages reduces.
+     * <br/> <br/>
+     * Set to 0 for no maximum.
+     *
+     * @param int $max_inflight_messages The maximum
+     */
+    public function setMaxInFlightMessages($max_inflight_messages) {}
+
+    /**
+     * Set the number of seconds to wait before retrying messages. This applies to publish messages with QoS>0. May
+     * be called at any time.
+     *
+     * @param int $message_retry
+     */
+    public function setMessageRetry($message_retry) {}
+
+    /**
+     * Publish a message on a given topic.
+     *
+     * @param string    $topic      The topic to publish on
+     * @param string    $payload    The message payload
+     * @param int       $qos        Integer value 0, 1 or 2 indicating the QoS for this message
+     * @param boolean   $retain     If true, make this message retained
+     */
+    public function publish($topic, $payload, $qos, $retain) {}
+
+    /**
+     * Subscribe to a topic.
+     *
+     * @param   string    $topic    The topic.
+     * @param   int       $qos      The QoS to request for this subscription
+     * @return  int                 Returns the message ID of the subscription message,
+     *                              so this can be matched up in the onSubscribe callback.
+     */
+    public function subscribe($topic, $qos) {}
+
+    public function unsubscribe
 }
